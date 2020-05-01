830ec3729a34045a89501cb5cac7437f2ca88d79
Verena Dittmer <Verena.Dittmer@cloudflight.io>
2020-05-01 15:31:54 +0200

    Add Wonders and Cooldown scripts

---
+++ Assets/Scripts/Wonders.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wonders : MonoBehaviour
+{
+    [SerializeField]
+    Cooldown prayCooldown;
+    [SerializeField]
+    Cooldown prayEffectCooldown;
+    private bool isPraying = false;
+    private bool isPrayEffectActive = false;
+
+
+    void Update()
+    {
+        if (isPraying && !prayCooldown.IsRunning())
+        {
+            EndPray();
+            StartPrayEffect();
+        }
+        if (isPrayEffectActive && !prayEffectCooldown.IsRunning())
+        {
+            EndPrayEffect();
+        }
+    }
+    
+    public void PrayPressed()
+    {
+        StartPray();
+    }
+
+    void StartPray()
+    {
+        Debug.Log("Start praying");
+        prayCooldown.ResetCooldown();
+        isPraying = true;
+        // TODO make good guys pray, i.e. stop and do nothing + animation
+    }
+
+    void EndPray()
+    {
+        Debug.Log("End praying");
+        isPraying = false;
+        // TODO make good guys pray
+    }
+
+    void StartPrayEffect()
+    {
+        Debug.Log("Start pray effect");
+        isPrayEffectActive = true;
+        prayEffectCooldown.ResetCooldown();
+    }
+
+    void EndPrayEffect()
+    {
+        Debug.Log("End pray effect");
+        isPrayEffectActive = false;
+    }
+}
