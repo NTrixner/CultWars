83b48873bc8be5ee0ef03271547dc57d24ac41c5
Tobias Watzl <twatzl@gmx.at>
2020-05-01 10:15:19 +0000

    Added basic ui.

---
+++ Assets/Code/FollowerCount.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class FollowerCount : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _text;
+    private int followers = 42;
+
+    void Update() {
+        // todo: add here the logic to update follower count
+        _text.text = followers.ToString();
+    }
+
+}
