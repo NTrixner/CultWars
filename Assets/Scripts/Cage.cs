85328383573e0ed47e876c5d6843fb4050f40fa2
Verena Dittmer <Verena.Dittmer@cloudflight.io>
2020-05-01 13:11:14 +0200

    Add cage prefab and script

---
+++ Assets/Scripts/Cage.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Cage : HealthEntity
+{
+    [SerializeField]
+    GameObject goodGuyPrefab;
+    [SerializeField]
+    int minGoodGuySpawn;
+    [SerializeField]
+    int maxGoodGuySpawn;
+
+    public override void Die()
+    {
+        float xmin = bodyCollider.bounds.min.x;
+        float xmax = bodyCollider.bounds.max.x;
+        float ymin = bodyCollider.bounds.min.y;
+        float ymax = bodyCollider.bounds.max.y;
+
+        Debug.Log("Cage died");
+        Destroy(gameObject);
+
+        int numGoodGuySpawn = Random.Range(minGoodGuySpawn, maxGoodGuySpawn + 1);
+        Debug.Log("Spawning " + numGoodGuySpawn + " good guys");
+        for (int i = 0; i < numGoodGuySpawn; i++)
+        {
+            float newx = Random.Range(xmin, xmax);
+            float newy = Random.Range(ymin, ymax);
+            GameObject goodGuy = Instantiate(goodGuyPrefab);
+            goodGuy.transform.position = new Vector3(newx, newy, 0.0f);
+        }
+    }
+}
