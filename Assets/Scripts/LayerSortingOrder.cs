4fa85c6cf0ad00cb28e7eab99d16584b5b80ab27
Verena Dittmer <Verena.Dittmer@cloudflight.io>
2020-05-01 21:35:47 +0200

    Add LayerSorting scripts to prefabs excluding Priest

---
+++ Assets/Scripts/LayerSortingOrder.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayerSortingOrder : MonoBehaviour
+{
+    private Rigidbody2D rigidbody;
+    private SpriteRenderer spriteRenderer;
+
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (rigidbody != null && spriteRenderer != null)
+        {
+            Debug.Log("Set Rigidbody and SpriteRenderer");
+            spriteRenderer.sortingOrder = (int) (rigidbody.position.y * -100);
+        }
+    }
+}
