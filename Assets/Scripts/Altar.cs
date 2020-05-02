56086d751944b5ad1ac7def002596ca6d9ea3417
ntrixner <n.trixner@gmx.at>
2020-05-02 12:22:45 +0200

    Add Altar + Prefab

---
+++ Assets/Scripts/Altar.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Altar : MonoBehaviour
+{
+    [SerializeField]
+    private List<SpriteRenderer> Relics;
+
+    [SerializeField]
+    private SpriteRenderer BigRelic;
+
+    [SerializeField]
+    private int currentRelic;
+
+    [SerializeField]
+    Collider2D collider;
+
+    void Start()
+    {
+        int i = 0;
+        while(i < Relics.Count)
+        {
+            Relics[i].enabled = i < currentRelic;
+            i++;
+        }
+        BigRelic.enabled = false;
+    }
+
+    public void GotRelic()
+    {
+        currentRelic++;
+        Relics[currentRelic-1].enabled = true;
+        if (currentRelic >= Relics.Count)
+        {
+            Debug.Log("You've won the Game!");
+            //[TODO] Win condition met! 
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        PlayerController controller;
+        if(collision.collider.TryGetComponent<PlayerController> (out controller))
+        {
+            //[TODO] Check if player has relic!
+            GotRelic();
+        }
+    }
+}
---
+++ Assets/Scripts/LayerSortingOrder.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayerSortingOrder : MonoBehaviour
+{
+    private Rigidbody2D body;
+    private SpriteRenderer spriteRenderer;
+
+    void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (body != null && spriteRenderer != null)
+        {
+            Debug.Log("Set Rigidbody and SpriteRenderer");
+            spriteRenderer.sortingOrder = (int)(body.position.y * -100);
+            return;
+        }
+        if (body == null && spriteRenderer != null)
+        {
+            Debug.Log("Set Rigidbody and SpriteRenderer");
+            spriteRenderer.sortingOrder = (int)(spriteRenderer.bounds.min.y * -100);
+            return;
+        }
+    }
+}
