4aa66e3423f1b082ad60fbe5e1137630545b8869
Tobias Watzl <twatzl@gmx.at>
2020-05-02 10:46:41 +0000

    Added tutorial ui prefab.

---
+++ Assets/Scripts/TutorialPanel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TutorialPanel : MonoBehaviour
+{
+    [SerializeField] private string title;
+
+    [SerializeField] private string content;
+
+    [SerializeField] private GameObject nextGameObjectShown;
+
+    [SerializeField] private TextMeshProUGUI titleMesh;
+    [SerializeField] private TextMeshProUGUI contentMesh;
+
+    [SerializeField] private Button button;
+
+    void OnEnable()
+    {
+        titleMesh.text = title;
+        contentMesh.text = content;
+        if (nextGameObjectShown != null) {
+            button.onClick.AddListener(() => nextGameObjectShown.SetActive(true));
+        }
+        button.onClick.AddListener(()=> gameObject.SetActive(false));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
